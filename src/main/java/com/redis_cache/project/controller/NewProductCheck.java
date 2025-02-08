4f089b0e9396636eb22f933bc184ba61125775ba
balajikothakota <bkothakota27@gmail.com>
2025-02-08 14:50:52 +0530

    test2

--- src/main/java/com/redis_cache/project/controller/NewProductCheck.java
+++ src/main/java/com/redis_cache/project/controller/NewProductCheck.java
@@ -1,4 +1,9 @@
 package com.redis_cache.project.controller;
 
+import java.sql.SQLOutput;
+
 public class NewProductCheck {
+    public void test(){
+        System.out.println("test branch....");
+    }
 }
